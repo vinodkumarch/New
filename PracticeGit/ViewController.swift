9ba8e2b2408d7c4c0ea49acb212a1d27b74add37
Ramyachoudhary <Ramyaanuvvula@gmail.com>
2019-01-24 17:20:07 +0530

    First Commit

---
+++ PracticeGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PracticeGit
+//
+//  Created by MXMACMINI1 on 24/01/19.
+//  Copyright © 2019 MB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("I am vinod")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
